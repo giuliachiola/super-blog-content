0e93412480fb3aa3061f6d88f556042c8b44b98e
Giulia Chiola <giulia.chiola.17@gmail.com>
2021-01-02 16:26:10 +0100

    feat: add projects permalink

--- projects/projects.11tydata.js
+++ projects/projects.11tydata.js
@@ -1,3 +1,6 @@
 module.exports = async () => ({
-  category: "projects"
+  category: "projects",
+
+  // this url is never called because at the moment does not exists a single page for each projects, but I prefer to have a clean `_site` directory with `projects/[nameofproject]`
+  permalink: 'projects/{{ title | slug }}/',
 })
