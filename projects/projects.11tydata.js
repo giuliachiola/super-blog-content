b1754b4a478d9449104e2065c19ba08082d17248
Giulia Chiola <giulia.chiola.17@gmail.com>
2021-01-10 12:03:26 +0100

    feat: add palette color in projects

--- projects/projects.11tydata.js
+++ projects/projects.11tydata.js
@@ -3,4 +3,5 @@ module.exports = async () => ({
   date: "Last Modified",
   category: "projects",
   permalink: 'projects/{{ title | slug }}/',
+  paletteColor: Math.floor((Math.random() * 5) + 1),
 })
