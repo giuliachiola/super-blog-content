25aac10c3794b7b7110c533ff8b6623827517e37
Giulia Chiola <giulia.chiola.17@gmail.com>
2021-06-15 08:57:21 +0200

    feat: remove permalink dynamic slug in 11tydata conifg

--- projects/projects.11tydata.js
+++ projects/projects.11tydata.js
@@ -2,6 +2,5 @@ module.exports = async () => ({
   layout: "layouts/post.njk",
   date: "Last Modified",
   category: "projects",
-  // permalink: 'projects/{{ title | slug }}/',
   paletteColor: Math.floor((Math.random() * 5) + 1),
 })
--- til/til.11tydata.js
+++ til/til.11tydata.js
@@ -2,7 +2,6 @@ module.exports = async () => ({
   layout: "layouts/post.njk",
   category: "til",
   date: "Last Modified",
-  // permalink: 'posts/{{ title | slug }}/',
   tags: 'post',
   paletteColor: Math.floor((Math.random() * 5) + 1),
 })
